8b7e6101526cec5549c074400d37abba5c7cd8cb
ayush-yadav-4 <officialayushyadav7@gmail.com>
2024-12-21 00:27:30 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
+
+
+void Swap(int a , int b , vector<int>& nums){
+    int temp = nums[a];
+    nums[a] = nums[b];
+    nums[b] = temp;
+}
     void moveZeroes(vector<int>& nums) {
-        int j =0,k=0;
+        int j =0;
+
         for(int i =0; i< nums.size();i++){
+          if(nums[i] == 0){
+            j =i;
+            break;
+          }
+          if(i == nums.size()-1){
+            return;
+          }
+        }
+
+        for(int i =j+1; i< nums.size();i++){
            if(nums[i]!=0){
-            nums[j] = nums[i];
+            Swap(j,i,nums);
             j++; 
            }
-           if(nums[i] == 0){
-            k++;
-           }
-        }
-         for(int i =0; i< k;i++){
-            nums.pop_back();
-         }
-         for(int i =0; i< k;i++){
-            nums.push_back(0);
-         }
-
+          
+      
+    }
     }
 };
