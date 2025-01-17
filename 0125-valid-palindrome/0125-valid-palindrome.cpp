dfc716498e3d4111818c0be2f8f7e075e87fc418
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-01-17 11:38:25 +0530

    Time: 9 ms (7.56%), Space: 9.9 MB (67.84%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-      int i = 0;
-        string ans;
-
-        while (s[i] != '\0') {
-            if (isalnum(s[i])) { // Check if alphanumeric
-                ans += tolower(s[i]); // Convert to lowercase and append
-            }
-            i++; // Increment i in every iteration
-        }
-
-        string ans1 = ans;
-        reverse(ans.begin(), ans.end());
-        return ans1 == ans;
+      int st = 0,end = s.size()-1;
+     
+         while(st<end){
+             if(!isalnum(s[st])){st++;continue;}
+             if(!isalnum(s[end])){end--; continue;}
+           else if(tolower(s[st]) != tolower(s[end])){
+           return false;
+           }
+           st++; end--;
+         }
+           return true;
+           
     }
 };
