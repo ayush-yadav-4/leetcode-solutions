fb2af3a7ead9dad2491ca1a57ebfb3270d25e060
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-01-19 15:44:46 +0530

    Time: 0 ms (100%), Space: 13.4 MB (61.25%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = -1;
-        map<int,int>mp;
-        for(int i =0; i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        for(auto i = mp.begin(); i != mp.end(); i++){
-           if(i->second == 1) return i->first;
+        int ans = 0,cnt = 0;
+        
+        
+        for(int bitset = 0; bitset<=31;bitset++){
+            cnt=0;
+            for(int i = 0 ; i< nums.size();i++){
+               if( (nums[i]&(1<<bitset))){
+                cnt++;
+               }
+            }
+            if (cnt%3 == 1){
+                ans = (ans | (1<<bitset));
+            }
         }
         return ans;
         
