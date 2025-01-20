98e23501b3cfc5b69c4308a8694041d096092132
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-01-20 17:14:45 +0530

    Time: 0 ms (100%), Space: 9.6 MB (71.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
-        if(s.size() ==1 &&t.size() == 1){if(s!=t)return false;}
-        int j=0;
-        for(int i=0;i<s.size()-1;i++){
-           if(s[i] != t[i]){
-            if(i == s.size()-1)return false;
-            j=i+1;
-            while(s[i]!=t[i]){
-                if(j==s.size()) return false;
-               swap(s[i],s[j]); 
-               j++;
-            }
-               
-           }
+      vector<int>arr(26,0);
+        for(int i=0;i<s.size();i++){
+           arr[s[i]-'a']++;
+           arr[t[i]-'a']--;
         }
-        if(s[s.size()-1]!= t[t.size()-1]) return false;
+
+        for(int i=0;i<arr.size();i++){
+           if(arr[i] != 0) return false;
+        }
+       
+      
         return true;
     }
 };
