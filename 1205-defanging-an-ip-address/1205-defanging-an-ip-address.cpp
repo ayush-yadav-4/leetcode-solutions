1569e0d69caad9a02c9a372854803816eb544689
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-02-09 18:58:03 +0530

    Time: 2 ms (51.47%), Space: 7.7 MB (77.26%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
@@ -4,9 +4,8 @@ public:
         string ans;int i=0;
         while(address[i] != '\0'){
             if(address[i] == '.'){
-                ans.push_back('[');
-                 ans.push_back('.');
-                   ans.push_back(']');
+                ans +="[.]";
+                
                    i++;
             }else{
                 ans.push_back(address[i]);
