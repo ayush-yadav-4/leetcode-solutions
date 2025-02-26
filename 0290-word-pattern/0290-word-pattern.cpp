1aa91a2fd2efca118a2851ccd878da142cecadf5
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-02-27 00:22:23 +0530

    Time: 0 ms (100%), Space: 9 MB (12.41%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,36 +1,50 @@
 class Solution {
 public:
-    bool wordPattern(string pattern, string s) {
-       unordered_map<char, string> charToWord;
-        unordered_map<string, char> wordToChar;
-        vector<string> words;
-        stringstream ss(s);
-        string word;
-
-       
-        while (ss >> word) {
-            words.push_back(word);
-        }
-
-        
-        if (pattern.size() != words.size()) {
-            return false;
-        }
-
-        for (int i = 0; i < pattern.size(); i++) {
-            char c = pattern[i];
-            string w = words[i];
-
-
-            if (charToWord.count(c)) {
-                if (charToWord[c] != w) return false; 
-            } else {
-                if (wordToChar.count(w)) return false; 
-                charToWord[c] = w;
-                wordToChar[w] = c;
+   void getwords(string s, vector<string>&words){
+     string word = "";
+     for(char c : s){
+        if(c == ' '){
+            if(word != ""){
+                words.push_back(word);
+                word = "";
             }
         }
+        else{
+          word += c;
+        }
+     }
 
-        return true;  
+     if(word != "") words.push_back(word);
+   }
+    bool wordPattern(string pattern, string s) {
+      vector<string>words;
+      getwords(s,words);
+      if(pattern.size() != words.size())return false;
+    
+      unordered_map<string , char>wordtochar;
+       unordered_map<char,string>chartoword;
+
+      for(int i=0;i<pattern.size();i++){
+         string word = words[i];
+         char c = pattern[i];
+
+         if(chartoword.find(c) != chartoword.end()){
+            if(chartoword[c] != word) return false;
+
+         }
+         
+         if(wordtochar.find(word) != wordtochar.end()){
+            if(wordtochar[word] != c) return false;
+
+         }
+
+         wordtochar[word] = c;
+         chartoword[c] = word;
+
+
+
+      }
+      return true;
+
     }
 };
