57bc8e91af5d003fc1a5b8227c019610571aec52
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-03-21 11:23:56 +0530

    Time: 0 ms (100%), Space: 13.1 MB (43.61%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
-        long long total =0;
+        long long int total =0;
         priority_queue<long long int >pq;
         for(int i=0;i<gifts.size();i++){
-          pq.push(gifts[i]);    
+          pq.push(gifts[i]);   
+          total += gifts[i];   
         }
         while(k--){
           long long int elem = pq.top();
           pq.pop();
-          
+          total -= (elem - (int)(sqrt(elem)));
           pq.push((int)(sqrt(elem)));
         }
-         while(!pq.empty()){
-            total += pq.top(); pq.pop();
-         }
+        
         
         return total;
 
