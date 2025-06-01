40b111bccf064803aeed086f74c564a0d848195b
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-06-01 10:32:41 +0530

    Time: 3 ms (76.19%), Space: 33.9 MB (45.45%) - LeetHub

--- 1479-construct-target-array-with-multiple-sums/1479-construct-target-array-with-multiple-sums.cpp
+++ 1479-construct-target-array-with-multiple-sums/1479-construct-target-array-with-multiple-sums.cpp
@@ -2,32 +2,27 @@ class Solution {
 public:
     bool isPossible(vector<int>& target) {
         
-        priority_queue<long long int>pq;
-        long long  int total=0,sum,remsum,maxelem,elem;
-         for(int i=0;i<target.size();i++){
-           pq.push(target[i]);
-          
-           total += target[i];
+       priority_queue<int>pq;
+       long long int sum=0,remsum=0,elem=0,maxelem=0;
+       for(int i=0;i<target.size();i++){
+            pq.push(target[i]);
+            sum += target[i];
+       }
+
+       while(pq.top() != 1){
+         maxelem = pq.top(); pq.pop();
+         remsum = sum - maxelem; 
+         if(remsum <= 0 || remsum >= maxelem) return false;
+
+         elem = maxelem % remsum;
+         if(elem == 0){
+            if(remsum == 1) return true;
+            else return false;
          }
-        while(pq.top() != 1){
-         maxelem = pq.top();
-         pq.pop();
+         sum = remsum + elem;
+         pq.push(elem);
+       }
 
-          remsum = total - maxelem;
-            
-            if(remsum >= maxelem || remsum <= 0) return 0;
-                elem = maxelem % remsum;
-                if(elem == 0){
-                    if(remsum != 1) return 0;
-                    else return 1;
-
-                }
-                
-                total =  remsum + elem;
-                 pq.push(elem);
-
-          
-        }
-        return 1;
+     return true;
     }
 };
