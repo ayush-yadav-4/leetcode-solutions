f457845d3623049f4c09b1b4af785550fce33f09
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-06-05 19:31:25 +0530

    Time: 20 ms (72.68%), Space: 70.6 MB (16.31%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
-bool Valid(vector<int>& bloomDay, int m, int k,int mid){
-  int cnt =0,total=0;
-    for(int i=0;i<bloomDay.size();i++){
-       if(bloomDay[i]<=mid){
-        cnt++;
-       }else{
-        total += cnt/k;
-        cnt =0;
-       }
-    }
-    total += cnt/k;
-
-    if(total>=m) return true;
-   return false;
-
-}
-    int minDays(vector<int>& bloomDay, int m, int k) {
-         int maxi =INT_MIN,mini=INT_MAX;
-        for(int i=0;i<bloomDay.size();i++){
-          maxi = max(maxi,bloomDay[i]);
-           mini = min(mini,bloomDay[i]);
+    bool check(vector<int>& arr, int m, int k, int mid) {
+        int f = 0, cnt = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] <= mid) {
+                cnt++;
+                if (cnt == k) {
+                    cnt = 0;
+                    f++;
+                }
+            } else if (f >= m)
+                return true;
+            else if (arr[i] > mid) {
+                cnt = 0;
+            }
         }
-        int st = mini,end = maxi,mid =0,ans =-1;
-        while(st<=end){
 
-         mid = st+(end-st)/2;
-         if(Valid(bloomDay,m,k,mid)){
-            ans = mid;
-            end = mid-1;
-         }
-         else{
-            st = mid+1;
-         }
+        if (f < m)
+            return false;
+        return true;
+    }
+    int minDays(vector<int>& arr, int m, int k) {
+        long long b = m,f = k;
+        if (arr.size() < (b * f))
+            return -1;
+        int ans = 0, mid = 0;
+        int st = INT_MAX, end = INT_MIN;
+        for (int i = 0; i < arr.size(); i++) {
+            st = min(st, arr[i]);
+            end = max(end, arr[i]);
+        }
 
-
+        while (st <= end) {
+            mid = st + (end - st) / 2;
+            if (check(arr, m, k, mid)) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
         }
         return ans;
     }
