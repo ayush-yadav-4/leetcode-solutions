038af7344406a339ff840b37b1145474320bd5f1
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-06-09 10:46:52 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.73%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size(), pivot = -1,idx = 0;
-        for(int i= n-2;i>=0;i--){
-           if(nums[i]<nums[i+1]){ pivot=i; break;}
+        // int n = nums.size(), pivot = -1,idx = 0;
+        // for(int i= n-2;i>=0;i--){
+        //    if(nums[i]<nums[i+1]){ pivot=i; break;}
            
+        // }
+        // if(pivot == -1){
+        //     sort(nums.begin(),nums.end());
+        //     return;
+        // }
+        // for(int i = n-1;i>=0;i--){
+        //    if(nums[i]>nums[pivot]){ idx = i; break;}
+        // }
+        // swap(nums[pivot],nums[idx]);
+        // reverse(nums.begin()+(pivot+1),nums.end());
+        // return;
+
+        //simpler solution .(idx after idx if found will be sorted)
+        // no need to sort the array after idx
+        int idx=-1,endidx=0;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx = i;
+                break;
+            }
         }
-        if(pivot == -1){
-            sort(nums.begin(),nums.end());
-            return;
+        if(idx != -1){
+         for(int i=nums.size()-1;i>=0;i--){
+            if(nums[i]>nums[idx]){
+                endidx = i;
+                break;
+            }
         }
-        for(int i = n-1;i>=0;i--){
-           if(nums[i]>nums[pivot]){ idx = i; break;}
+        
+
+        swap(nums[idx],nums[endidx]);
         }
-        swap(nums[pivot],nums[idx]);
-        reverse(nums.begin()+(pivot+1),nums.end());
-        return;
+        reverse(nums.begin()+idx+1,nums.end());
     }
 };
