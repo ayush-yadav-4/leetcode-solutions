f2d008ecab103f004189386d58faacc4d9d30b50
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-06-11 11:27:05 +0530

    Time: 4 ms (69.65%), Space: 23.8 MB (59.12%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.size() <= 1) return intervals;
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        // if (intervals.size() <= 1) return intervals;
 
     
-        sort(intervals.begin(), intervals.end());
+        // sort(intervals.begin(), intervals.end());
 
-        vector<vector<int>> ans;
-        vector<int> temp = intervals[0]; 
+        // vector<vector<int>> ans;
+        // vector<int> temp = intervals[0]; 
 
-        for (const auto& interval : intervals) {
-            if (interval[0] <= temp[1]) {
+        // for (const auto& interval : intervals) {
+        //     if (interval[0] <= temp[1]) {
                
-                temp[1] = max(temp[1], interval[1]);
-            } else {
+        //         temp[1] = max(temp[1], interval[1]);
+        //     } else {
                
-                ans.push_back(temp);
-                temp = interval;
+        //         ans.push_back(temp);
+        //         temp = interval;
+        //     }
+        // }
+
+        // ans.push_back(temp);
+        // return ans;
+
+         vector<vector<int>>ans;
+        sort(arr.begin(),arr.end());
+        ans.push_back({arr[0][0],arr[0][1]});
+        for(int i=1;i<arr.size();i++){
+             if(!ans.empty() && ans.back()[1] >= arr[i][0]){
+                ans.back()[1] = max(arr[i][1],ans.back()[1]);
+             }
+            else{
+                ans.push_back({arr[i][0],arr[i][1]});
             }
         }
-
-        ans.push_back(temp);
         return ans;
+
+        //TC = O(N*LognN) sorting array + O(N)  Traversal
     }
 };
