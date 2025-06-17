8ee74495915d74862997fcdd4ec62baca876b58d
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-06-17 12:36:15 +0530

    Time: 0 ms (100%), Space: 10.3 MB (27.87%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,38 +1,62 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        if(num1.size() > num2.size()){
-            return add(num1,num2);
-        }
-        else{
-            return add(num2,num1);
-        }
+    //     if(num1.size() > num2.size()){
+    //         return add(num1,num2);
+    //     }
+    //     else{
+    //         return add(num2,num1);
+    //     }
       
-    }
+    // }
 
-    string add(string num1,string num2){
+    // string add(string num1,string num2){
 
-        int carry =0, idx1 = num1.size()-1, idx2 = num2.size()-1,sum=0;
+    //     int carry =0, idx1 = num1.size()-1, idx2 = num2.size()-1,sum=0;
+    //     string ans;
+    //       while(idx2 >= 0){
+    //       sum = (num1[idx1] - '0') + (num2[idx2] - '0') + carry;
+    //       carry = sum/10;
+    //       char c =  '0' +(sum%10 );
+    //       ans += c;
+    //       idx1--; idx2--;
+    //     }
+
+    //      while(idx1 >= 0){
+    //       sum = (num1[idx1] - '0') + carry;
+    //       carry = sum/10;
+    //      char c =  '0' +(sum%10 );
+    //       ans += c;
+    //        idx1--;
+    //     }
+    //     if(carry) ans += '1';
+
+    //     reverse(ans.begin(), ans.end());
+
+    //     return ans;
+
+     int i = num1.size()-1,j=num2.size()-1,carry=0,sum=0;
         string ans;
-          while(idx2 >= 0){
-          sum = (num1[idx1] - '0') + (num2[idx2] - '0') + carry;
-          carry = sum/10;
-          char c =  '0' +(sum%10 );
-          ans += c;
-          idx1--; idx2--;
+        while(i>=0 && j>=0){
+           sum = (num1[i]-'0' + num2[j] - '0' + carry);
+           carry = sum/10;
+           ans += to_string(sum%10);
+           i--;j--;
+        } 
+        while(j>=0){
+            sum = (num2[j] - '0' + carry);
+           carry = sum/10;
+           ans += to_string(sum%10);
+           j--;
         }
-
-         while(idx1 >= 0){
-          sum = (num1[idx1] - '0') + carry;
-          carry = sum/10;
-         char c =  '0' +(sum%10 );
-          ans += c;
-           idx1--;
+        while(i>=0){
+            sum = (num1[i] - '0' + carry);
+           carry = sum/10;
+           ans += to_string(sum%10);
+           i--;
         }
-        if(carry) ans += '1';
-
-        reverse(ans.begin(), ans.end());
-
+        if(carry != 0){ ans += to_string(carry);}
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
