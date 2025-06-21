662d4c99e97839c7c25eceb431cf16d6ede3d23c
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-06-21 10:54:05 +0530

    Time: 11 ms (23.13%), Space: 18.7 MB (96.87%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,37 +1,65 @@
 class Solution {
 public:
-  bool loop(vector<vector<int>>& graph,vector<int>&visited,int idx){
-    if(visited[idx] == 1) return false; // currently processing and Loop detect
-    if(visited[idx] == 2) return true;
-     visited[idx] = 1;
-    for(auto it : graph[idx]){
+    bool loop(vector<vector<int>>& graph, vector<int>& visited, int idx) {
+        if (visited[idx] == 1)
+            return false; // currently processing and Loop detect
+        if (visited[idx] == 2)
+            return true;
+        visited[idx] = 1;
+        for (auto it : graph[idx]) {
 
-         if(loop(graph,visited,it) == false) return false;
-         
-           
-        
+            if (loop(graph, visited, it) == false)
+                return false;
+        }
+        visited[idx] = 2;
+        return true;
     }
-    visited[idx] = 2;
-     return true;
+    
+    bool DFS(vector<int>adj[],vector<int>&vis,vector<int>&pathvis,int node){
+    vis[node] = 1;
+    pathvis[node] = 1;
+    for(auto it : adj[node]){
+       if(vis[it] == 1 && pathvis[it] == 1) return true;
+       else if(!vis[it]){
+        if(DFS(adj,vis,pathvis,it)) return true;
+       }
+    }
+    pathvis[node] = 0;
 
-  }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>visited(numCourses,0);
-     
-        vector<vector<int>>adj(numCourses);
+    return false;
+ }
+    bool canFinish(int V, vector<vector<int>>& pre) {
+        // vector<int> visited(numCourses, 0);
 
-        for(int i=0;i<prerequisites.size();i++){
-          int u = prerequisites[i][0];
-          int v = prerequisites[i][1];
-          adj[u].push_back(v);
+        // vector<vector<int>> adj(numCourses);
+
+        // for (int i = 0; i < prerequisites.size(); i++) {
+        //     int u = prerequisites[i][0];
+        //     int v = prerequisites[i][1];
+        //     adj[u].push_back(v);
+        // }
+
+        // for (int i = 0; i < numCourses; i++) {
+
+        //     if (loop(adj, visited, i) == false)
+        //         return false;
+        // }
+
+        // return true;
+
+            vector<int>adj[V];
+        vector<int>vis(V,0);
+         vector<int>pathvis(V,0);
+        for(int i=0;i<pre.size();i++){
+          adj[pre[i][1]].push_back(pre[i][0]);
+          
         }
 
-        for(int i=0;i<numCourses;i++){
-        
-                if(loop(adj,visited,i) == false) return false;
-            
+        for(int i=0;i<V;i++){
+          if(!vis[i]){
+            if(DFS(adj,vis,pathvis,i)) return false;
+          }
         }
-       
         return true;
     }
 };
