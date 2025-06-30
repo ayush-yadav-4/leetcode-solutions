c5b2cf0b503e22026b8f8a114a943f71f91049a6
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-06-30 23:06:08 +0530

    Time: 16 ms (70.93%), Space: 38.6 MB (48.41%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,78 +1,42 @@
 class Solution {
-
-    //with matrix - 
-    //  int rows = mat.size();
-    //     int cols = mat[0].size();
-    //     vector<vector<int>> dist(rows, vector<int>(cols, INT_MAX - 1)); // Avoid overflow
-
-    //     // First Pass: Top-Left to Bottom-Right
-    //     for (int i = 0; i < rows; ++i) {
-    //         for (int j = 0; j < cols; ++j) {
-    //             if (mat[i][j] == 0) {
-    //                 dist[i][j] = 0;
-    //             } else {
-    //                 if (i > 0) {
-    //                     dist[i][j] = min(dist[i][j], dist[i - 1][j] + 1);
-    //                 }
-    //                 if (j > 0) {
-    //                     dist[i][j] = min(dist[i][j], dist[i][j - 1] + 1);
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     // Second Pass: Bottom-Right to Top-Left
-    //     for (int i = rows - 1; i >= 0; --i) {
-    //         for (int j = cols - 1; j >= 0; --j) {
-    //             if (mat[i][j] == 1) {
-    //                 if (i < rows - 1) {
-    //                     dist[i][j] = min(dist[i][j], dist[i + 1][j] + 1);
-    //                 }
-    //                 if (j < cols - 1) {
-    //                     dist[i][j] = min(dist[i][j], dist[i][j + 1] + 1);
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     return dist;
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-       
+        
+        vector<vector<int>>visited(mat.size(),vector<int>(mat[0].size(),0));
        queue<pair<int,pair<int,int>>>q;
-       vector<vector<int>>visited(mat.size(),vector<int>(mat[0].size(),0));
-       vector<vector<int>>ans(mat.size(),vector<int>(mat[0].size(),0));
+       for(int i =0 ;i<mat.size();i++){
+          for(int j =0 ;j<mat[0].size();j++){
+              if(mat[i][j] == 0){
+               
+                visited[i][j] =1;
+                q.push({i,{j,0}});
+              }
+              else{
+                    
+              }
+          }
+        }
+        int rows[] = {0,0,-1,+1};
+        int cols[] = {-1,+1,0,0};
+        while(!q.empty()){
+          int r = q.front().first;
+          int c = q.front().second.first;
+          int wt = q.front().second.second;
+           q.pop();
+              mat[r][c] = wt;
 
-       for(int i=0;i<mat.size();i++){
-         for(int j=0;j<mat[i].size();j++){
-           if(mat[i][j] == 0){
-            q.push({0,{i,j}});
-            visited[i][j] = 1;
-           }else{
-            visited[i][j] = 0;
-           }
-         }
-       }
-     int row[] ={0,0,+1,-1};
-     int col[] = {-1,+1,0,0}; 
-      while(!q.empty()){
-         int r = q.front().second.first;
-         int c = q.front().second.second;
-         int occr = q.front().first;
-         ans[r][c] = occr;
-         q.pop();
+          for(int i=0;i<4;i++){
+             int row = r + rows[i];
+              int col = c + cols[i];
+             
 
-         for(int i=0;i<4;i++){
-          int nrow = r + row[i];
-          int ncol = c+ col[i];
+              if(row>=0 && row < mat.size() && col>=0 && col<mat[0].size() && visited[row][col] == 0){
+                visited[row][col] = 1;
+                q.push({row,{col,wt+1}});
 
-          if(nrow >=0 && nrow < mat.size() && ncol >=0 && ncol < mat[0].size() && visited[nrow][ncol]== 0 ){
-              visited[nrow][ncol] = 1;
-              q.push({occr+1,{nrow,ncol}});
-          } 
-         }
-       
-      }
-        return ans;
+              } 
+          }
+        }
+        return mat;
     }
 };
