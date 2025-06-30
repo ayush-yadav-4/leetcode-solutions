fe655c74b61ae5ffc4a4c83a0b70d2884683b361
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-06-30 19:50:41 +0530

    Time: 39 ms (17.13%), Space: 19.1 MB (15.75%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-         vector<vector<int>>ans;
-         int start=0,end=0; long long sum=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(i>0 && nums[i] == nums[i-1])continue;
-            for(int j=i+1;j<nums.size();j++){
-               if(j>i+1 && nums[j] == nums[j-1])continue;
-               start = j+1; end = nums.size()-1;
-               while(start<end){
-                sum = (long long)nums[i] + (long long)nums[j] + 
-                          (long long)nums[start] + (long long)nums[end]; 
-
-                if(sum<target){
-                    start++;
+          sort(nums.begin(),nums.end());
+        vector<vector<int>>ans; int st=0, end=0;
+        long long int elem =0 ;
+         if(nums.size() <4) return ans;
+        set<vector<int>>set;
+        for(int i=0;i<nums.size()-2;i++){
+            // if(i>0 && nums[i] == nums[i-1]){
+            //     continue;
+            // }
+            for(int j=i+1;j<nums.size()-1;j++){
+            //      if(j>0 && nums[j] == nums[j-1]){
+            //     continue;
+            // }
+            st = j+1;end = nums.size()-1;
+                while(st < end){
+                   elem = (nums[i] + nums[j]) ;
+                   if(target>(elem+nums[st] + nums[end])){
+                    st++;
+                   }
+                   else if(target<(elem + nums[st] + nums[end])) end--;
+                   else{
+                    //ans.push_back({nums[i],nums[j],nums[st],nums[end]});
+                    set.insert({nums[i],nums[j],nums[st],nums[end]});
+                    st++; end--;
+                    while(nums[st] == nums[st-1] && st < end){
+                      st++;
+                    }
+                   }
                 }
-                else if(sum>target)end--;
-               
-               else {
-                ans.push_back({nums[i],nums[j],nums[start],nums[end]});
-                start++;end--;
-                while( start<end && nums[start] == nums[start-1])start++;
-               }
-            }
             }
         }
+       for(auto x : set){
+         ans.push_back(x);
+       }
         return ans;
     }
 };
