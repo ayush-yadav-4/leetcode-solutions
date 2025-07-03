7d2810439a445bc60c836d476b44d93bd8b320ef
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-07-03 14:52:27 +0530

    Time: 11 ms (44.04%), Space: 34.4 MB (52.55%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,46 +1,64 @@
 class Solution {
 public:
-void dfs(int r,int c,vector<vector<int>>&visited,vector<vector<int>>&grid){
-  visited[r][c] = 1;
-  int nrow[] = {0,0,-1,+1};
-  int ncol[] = {-1,+1,0,0};
-  for(int i=0;i< 4 ;i++){
-    int row = r+ nrow[i];
-    int col = c+ ncol[i];
-    if(row>=0 && row<grid.size() && col >=0 && col < grid[0].size() && grid[row][col] == 1 && visited[row][col] == 0){
-       visited[row][col] = 1;
-       dfs(row,col,visited,grid);
-    }
-  }
+
+void BFS(vector<vector<int>>& grid,vector<vector<int>>& vis,int row,int col){
+ 
+   queue<pair<int,int>>q;
+   q.push({row,col});
+   vis[row][col] = 1;
+
+ int rows[] = {0,0,-1,+1};
+     int cols[] = {-1,+1,0,0};
+   while(!q.empty()){
+     int r = q.front().first;
+     int c = q.front().second;
+     q.pop();
+     
+     
+    
+     for(int i=0;i<4;i++){
+       int ro = r + rows[i];
+       int co = c + cols[i];
+      if(ro>=0 && co<grid[0].size()-1 && co>=0 && ro<grid.size()-1 && vis[ro][co] == 0 && grid[ro][co] == 1){
+         vis[ro][co]= 1;
+        q.push({ro,co});
+        
+        
+      } 
+
+     }
+   }
 }
     int numEnclaves(vector<vector<int>>& grid) {
-        vector<vector<int>>visited(grid.size(),vector<int>(grid[0].size(),0));
+        
 
-        for(int j = 0 ;j< grid[0].size();j++){
-           if(grid[0][j] == 1 && visited[0][j] == 0){
-            dfs(0,j,visited,grid);
-           }
-           if(grid[grid.size()-1][j] == 1 && visited[grid.size()-1][j] == 0){
-            dfs(grid.size()-1,j,visited,grid);
-           }
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+
+        
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 1 && vis[i][0] == 0)
+                BFS(grid, vis, i, 0);
+            if (grid[i][n - 1] == 1 && vis[i][n - 1] == 0)
+                BFS(grid, vis, i, n - 1);
         }
 
-           for(int i = 0 ;i< grid.size();i++){
-           if(grid[i][0] == 1 && visited[i][0] == 0){
-            dfs(i,0,visited,grid);
-           }
-           if(grid[i][grid[0].size()-1] == 1 && visited[i][grid[0].size()-1] == 0){
-            dfs(i,grid[0].size()-1,visited,grid);
-           }
+        
+        for (int j = 0; j < n; j++) {
+            if (grid[0][j] == 1 && vis[0][j] == 0)
+                BFS(grid, vis, 0, j);
+            if (grid[m - 1][j] == 1 && vis[m - 1][j] == 0)
+                BFS(grid, vis, m - 1, j); 
         }
-         int ans = 0;
+
+        int cnt = 0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-              if(grid[i][j] == 1 && visited[i][j] == 0){
-                ans++;
-              }
+               if(grid[i][j] == 1 && vis[i][j] == 0){
+                  cnt++;
+               }
             }
         }
-        return ans;
+        return cnt;   
     }
 };
