81e3655399453f2ace151594a1db7060978f6b58
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-07-08 00:02:20 +0530

    Time: 1 ms (52.65%), Space: 19.4 MB (75.04%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,25 +1,55 @@
+
+class Disjoint{
+
+public:
+vector<int> size,par;
+  Disjoint(int v){
+    size.resize(v,1);
+    par.resize(v);
+    for(int i=0;i<v;i++){
+         par[i] = i;
+    }
+  }
+
+  int findparent(int u){
+    if(par[u] == u) return u;
+
+    return par[u] = findparent(par[u]);
+  }
+
+  void unionbysize(int u,int v){
+      int par_u = findparent(u);
+       int par_v = findparent(v);
+
+       if(par_u == par_v) return;
+       else if(size[par_u] > size[par_v]){
+           par[par_v] = par_u;
+           size[par_u] += size[par_v];
+       }
+       else{
+         par[par_u] = par_v;
+           size[par_v] += size[par_u];
+       }
+
+  }
+};
+
 class Solution {
 public:
-void dfs(int idx , vector<vector<int>>& isConnected,vector<int>& visited){
-    visited[idx] = 1;
-    for(int i=0;i<isConnected[0].size();i++){
-        if(i != idx && isConnected[idx][i] == 1 && visited[i] == 0){       
-            visited[i] = 1;
-            dfs(i,isConnected,visited);
- }
-    }
-    
-}
     int findCircleNum(vector<vector<int>>& isConnected) {
-        vector<int>visited(isConnected.size(),0);
-        int ans = 0;
         
-        
-        for(int i =0 ;i<isConnected.size();i++){
-            if(visited[i] == 0){ ans++;
-            dfs(i,isConnected,visited);
+       Disjoint d(isConnected.size());
+        for(int i = 0;i<isConnected.size();i++){
+            for(int j = 0;j<isConnected[0].size();j++){
+              if(isConnected[i][j] == 1){
+                d.unionbysize(i,j);
+              }
             }
         }
-        return ans;
+        int cnt = 0;
+        for(int i=0;i<isConnected.size();i++){ 
+           if(d.findparent(i) == i)cnt++;
+        }
+        return cnt;
     }
 };
