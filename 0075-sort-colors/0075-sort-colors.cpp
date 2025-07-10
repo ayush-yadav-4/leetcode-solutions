f545da50ebdb4574c425899c1be6653f94cea854
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-07-10 16:21:13 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-void Swap(int a , int b, vector<int>& nums){
-   int temp = nums[a];
-   nums[a] = nums[b];
-   nums[b] = temp;
-}
     void sortColors(vector<int>& nums) {
-        // int mid =0 ,low=0, high = nums.size()-1;
-           
-        // while(mid<=high){
-
-        //     if(nums[mid] == 0){
-        //         Swap(mid,low,nums);
-        //         mid++;low++;
-        //     }
-        //     else if(nums[mid] == 1){
-        //         mid++;
-        //     }
-        //     else{
-        //         Swap(mid,high,nums);
-        //         high--;
-        //     }
-        // }
-
+        if(nums.size() == 1) return;
+        int st = 0,mid = 0, end = nums.size()-1;
+        while(mid<=end && mid<nums.size()){
+          
+         if(nums[mid] == 0){
+            swap(nums[mid],nums[st]);
+            st++; mid++;
+         }
+         else if(nums[mid] == 1){
+            mid++;
+         }
+         else if(nums[mid] == 2){
+            swap(nums[mid],nums[end]);
+            end--;
+         }
+          
         
-        int st=0,mid=0,end = nums.size()-1;
-        while(mid<=end){
-              if(nums[mid] == 1)mid++;
-              else if (nums[mid] == 0){ swap(nums[mid],nums[st]); mid++;st++;}
-              else{ swap(nums[mid],nums[end]); end--;}
+         
+         
 
         }
+        return;
     }
 };
