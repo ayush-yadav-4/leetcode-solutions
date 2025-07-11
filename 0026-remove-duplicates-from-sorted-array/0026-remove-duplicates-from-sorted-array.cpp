bce17441a16cd3a53e518ea357a81920f8e80c9a
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-07-11 14:52:46 +0530

    Time: 0 ms (100%), Space: 22.7 MB (20.59%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    void Swap(int a, int b,vector<int>& nums){
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-    }
     int removeDuplicates(vector<int>& nums) {
-        int idx=0,k=1;
-        vector<int>ans;
-        int i=0, j=1;
-      
-         for(int j =0; j < nums.size();j++){
-           if(nums[i] != nums[j]){
-            nums[i+1] = nums[j];
-            i++;
-            k++;
-           }
-         }
-       
-    return k;
+        //   int cnt = 0;
+        //   set<int>st;
+        //   vector<int>ans;
+
+        //  for(int i=0;i<nums.size();i++){
+        //     if(st.find(nums[i]) == st.end()){
+        //         st.insert(nums[i]);
+        //     }
+           
+
+        //  } 
+        //  int i=0;
+        //  for(auto it : st){
+        //    nums[i] = it;
+        //    i++;
+        //  }
+        //  return st.size();
+        int i=0;
+     for(int j=1;j<nums.size();j++){
+          if(nums[i] != nums[j]){
+             i++;
+            nums[i] = nums[j];
+           
+          }
+     }
+         return i+1;
     }
 };
