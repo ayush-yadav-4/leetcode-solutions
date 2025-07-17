268e7131c076749278e05ce858412f1769a38fdd
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-07-17 10:11:01 +0530

    Time: 15 ms (22.75%), Space: 22.8 MB (73.94%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
 bool check(vector<int>& piles, int h,int mid){
-    long long int ans = 0;
-   for(int i=0;i<piles.size();i++){
-        
-          if(piles[i]%mid == 0){
-            ans += piles[i]/mid;
-          }else  ans += (piles[i]/mid) + 1;          
-         }
-         if(ans > h) return false;
-         return true;
+   int ans = 0;
+   for(int it : piles){
+      if(it % mid == 0){
+        ans += (it/mid);
+      } 
+      else{
+        ans += (it/mid)+1;
+      }
+      if(ans > h) return false;
+   }
+   if(ans > h) return false;
+   return true;
 }
     int minEatingSpeed(vector<int>& piles, int h) {
-          int st = 1,end=0,ans=0,mid=0;
-         for(int i=0;i<piles.size();i++){
-           end = max(end, piles[i]);          
-         }
-         
-         while(st<=end){
-            mid = st + (end-st)/2;
-            if(check(piles,h,mid)){
-                ans = mid;
-                end = mid-1;
-            }
-            else st = mid+1;
-         } 
-         return ans;          
-    }                                       
+        long long int  st =1,end = 0,mid=0;
+        int ans = 0;
+
+        for(int it: piles){
+          end += it;
+        }
+        while(st<=end){
+           mid = (st + (end-st)/2);
+
+           if(check(piles,h,mid)){
+            ans = mid;
+            end = mid-1;
+           }
+           else{
+            st = mid+1;
+           }
+        }
+        return ans;
+    }
 };
