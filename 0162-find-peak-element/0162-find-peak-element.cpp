37181be262513a2d6982c60343979f395245fc36
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-07-17 09:35:24 +0530

    Time: 0 ms (100%), Space: 12.4 MB (67.27%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-         if(nums.size() == 1){
-            return nums.size()-1;
-         }
-        int start=0,end=nums.size()-1,mid=0,peak =0,maxi=0;
-       while(start <= end){
-        mid = start + (end-start)/2;
-        if(mid == start ){
-            if(nums[mid] > nums[mid +1]){
-                return mid;
-            }else{
-                return mid +1;
-            }
+        if(nums.size() == 1){
+           return 0;
         }
-         if(mid == start ){
-            if(nums[mid] > nums[mid -1]){
-                return mid;
-            }else{
-                return mid -1;
-            }
+
+         if(nums.size() == 2){
+           if(nums[0] > nums[1]) return 0;
+           else return 1;
         }
-       
-        if(nums[mid] > nums[mid+1] && nums[mid]>nums[mid-1]){
-            peak = mid;
-            return mid;
-        }
-        else if(nums[mid] < nums[mid+1] && nums[mid]>nums[mid-1]){
-            start = mid +1;
-        }else{
+        int st = 1,end = nums.size()-2,mid=0;
+        if(nums[0] > nums[st]) return 0;
+        if(nums[nums.size()-1] > nums[end]) return nums.size()-1;
+        while(st<=end){
+          mid = st + (end-st)/2;
+          if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+             return mid;
+          }
+         if(nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]){
+             st = mid+1;
+          }
+          else {
             end = mid-1;
+          }
         }
-        
-       }
-        return peak;
+        return mid;
     }
 };
