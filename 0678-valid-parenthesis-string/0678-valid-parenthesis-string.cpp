840366939b598d46881c09eaae01a8eccb2d6e37
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-07-22 19:18:58 +0530

    Time: 0 ms (100%), Space: 8 MB (74.25%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-    
-    int i=0,left=0,right=0;
-      while(s[i] != '\0'){
-        char a = s[i];
-        if(a == '('){left++;right++;}
-        else if(a == ')'){
-            left--; right--;
-        }else{
-           left--;
-           right++;
-        }
-        if(left<0) left = 0;
-        if(right<0) return false;
-        i++;
-      } 
-     
-      
-        return left == 0;
         
-    }
+        
+        int min=0,max=0,i=0;
+        while(s[i] != '\0'){
+           if(s[i] == '('){
+            max = max + 1;
+            min = min + 1;
+           }
+           else if(s[i] == ')'){
+             max = max - 1;
+             min = min -1;
+           }
+           else{
+            min = min -1;
+            max = max + 1;
+           }
+           if(min<0)min = 0;
+           i++;
+           if(max < 0) return false;
+        }
+        
+
+        return (min == 0);   
+         }
 };
