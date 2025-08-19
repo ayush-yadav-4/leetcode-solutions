81e845d7167e65b42d041256d5e6674b9ab90a26
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-08-19 23:55:53 +0530

    Time: 10 ms (82.67%), Space: 44.9 MB (95.77%) - LeetHub

---
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int prev = 0, total = 0;
+        
+        for (String row : bank) {
+            int curr = 0;
+            for (char c : row.toCharArray()) {
+                if (c == '1') curr++;
+            }
+            
+            if (curr > 0) {
+                total += prev * curr;
+                prev = curr; // update previous non-empty row
+            }
+        }
+        
+        return total;
+    }
+}
