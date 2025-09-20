ef634ce3f9b251cfc938b633e989d67cf63ea542
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-09-20 17:13:26 +0530

    Time: 8 ms (24.34%), Space: 76.6 MB (7.67%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-         vector<int>ans(arr.size(),-1);
-         int elem  = arr[arr.size()-1];
-         arr[arr.size()-1] = arr[arr.size()-1];
-        for(int i=arr.size()-2;i>=0;i--){
-           elem = max(elem,arr[i+1]);
-           ans[i] = elem;
-        }
-        return ans;
+         vector<int> ans;
+         ans.push_back(-1);
+         int elem = arr[arr.size()-1];
+         for(int i=arr.size()-2;i>=0;i--){
+             
+              
+              
+               ans.push_back(elem);
+                elem = max(elem,arr[i]);
+
+         }
+         reverse(ans.begin(),ans.end());
+         return ans;
     }
 };
