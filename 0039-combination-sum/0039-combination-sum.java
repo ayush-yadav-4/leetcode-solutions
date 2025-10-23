6fdd75c8b8238fa851d5f61a227aea2053727fcb
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-10-23 11:05:45 +0530

    Time: 2 ms (84.64%), Space: 44.8 MB (31.64%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,37 +1,28 @@
-
-
 class Solution {
-
-    public static void helper(List<List<Integer>> ans, List<Integer> temp, int[] nums, int target, int idx) {
-        if (target == 0) {
+    void helper( List<List<Integer>> ans, List<Integer>temp,int target,int[] nums,int idx){
+        if(idx == nums.length){
+            if(target == 0){
             ans.add(new ArrayList<>(temp));
             return;
         }
-        if (idx == nums.length || target < 0) {
+        return;
+        }
+        else if(target<0) return;
+        else if(target ==0){
+            ans.add(new ArrayList<>(temp));
             return;
         }
-
-        // include nums[idx]
         temp.add(nums[idx]);
-        helper(ans, temp, nums, target - nums[idx], idx); // stay at same idx (can reuse element)
-        temp.remove(temp.size() - 1);
-
-        // exclude nums[idx] and move to next
-        helper(ans, temp, nums, target, idx + 1);
+        helper(ans,temp,target-nums[idx],nums,idx);
+        temp.remove(temp.size()-1);
+        helper(ans,temp,target,nums,idx+1);
+        
     }
-
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        helper(ans, temp, nums, target, 0);
-        return ans;
-    }
+        List<Integer>temp = new ArrayList<>();
 
-    // For testing
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[] nums = {2, 3, 6, 7};
-        int target = 7;
-        System.out.println(sol.combinationSum(nums, target));
+        helper(ans,temp,target,candidates,0);
+        return ans;
     }
 }
