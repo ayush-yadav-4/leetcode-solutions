eacb882024c5c8644cf1b0c8edc82834ace57f0e
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-10-28 23:58:09 +0530

    Time: 96 ms (24.54%), Space: 55.7 MB (45.4%) - LeetHub

---
+++ 1808-stone-game-vii/1808-stone-game-vii.java
@@ -0,0 +1,15 @@
+class Solution {
+    int dp[][];
+    public int stoneGameVII(int[] stones) {
+        dp = new int[stones.length][stones.length];
+        return score(stones, 0, stones.length - 1, Arrays.stream(stones).sum());
+    }
+    int score(int[] stones, int l, int r, int sum) {
+        if(l + 1 == r) return Math.max(stones[l], stones[r]);
+        if(dp[l][r] != 0) return dp[l][r];
+        int left = score(stones, l + 1, r, sum - stones[l]);
+        int right = score(stones, l, r - 1, sum - stones[r]);
+        dp[l][r] = Math.max(sum - stones[l] - left, sum - stones[r] - right);
+        return dp[l][r];
+    }
+}
