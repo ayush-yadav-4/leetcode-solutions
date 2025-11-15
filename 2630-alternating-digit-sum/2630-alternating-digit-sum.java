cd3e5bdcaf4978bd19ef7d265759dd95f26c5214
ayush-yadav-4 <officialayushyadav7@gmail.com>
2025-11-15 23:52:10 +0530

    Time: 0 ms (100%), Space: 42.1 MB (9.21%) - LeetHub

---
+++ 2630-alternating-digit-sum/2630-alternating-digit-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        int sum = 0,cons = 1,ne= 0;
+        while(n>0){
+            ne = ne *10 + n%10;
+            n /= 10;
+        }
+        while(ne>0){
+            sum = sum + (ne%10 * cons);
+            ne /= 10;
+            cons *= -1;
+        }
+        return sum;
+    }
+
+}
