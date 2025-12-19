21a89aa1fdc225d138a9e17c66b1bbb2796b584b
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2025-12-19 21:26:09 +0530

    Time: 0 ms (100%), Space: 35.7 MB (54.54%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-       
+      
         int n = nums.size();
-        vector<int>suffix(nums.size(),0);
-        vector<int>prefix(nums.size(),0);
+        // vector<int>suffix(nums.size(),0);
+        // vector<int>prefix(nums.size(),0);
         // prefix[0] = nums[0];
         // suffix[n-1] = nums[n-1];
-
-        for(int i = 1;i<nums.size();i++){
-            prefix[i] = nums[i-1] + prefix[i-1];
-            
-        }
-
-         for(int i = n-2;i>=0;i--){
-            suffix[i] = nums[i+1] + suffix[i+1];
-            
-        }
+        int sum = 0;
+        int suffix = 0, prefix = 0;
+         for(int arr : nums){
+          sum += arr;
+         }
         for(int i = 0;i<nums.size();i++){
-          if(prefix[i] == suffix[i]) return i;
+          
+          int suffix = sum - prefix - nums[i];
+    if(prefix == suffix) return i;
+    prefix += nums[i];
+            
         }
+
+        //  for(int i = n-2;i>=0;i--){
+        //     suffix[i] = nums[i+1] + suffix[i+1];
+            
+        // }
+        // for(int i = 0;i<nums.size();i++){
+        //   if(prefix[i] == suffix[i]) return i;
+        // }
 
         return -1;
     }
