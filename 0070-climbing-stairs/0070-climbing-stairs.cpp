74c87c200036b8460c5a90af8f3dfddd6ac67aaf
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2025-12-24 15:08:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (16.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -12,6 +12,17 @@ int helper(int n,vector<int>&dp){
 }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return helper(n,dp);
+
+        //return helper(n,dp);
+       if(n == 0 || n ==1 || n==2) return n;
+        dp[0] =0;
+        dp[1] = 1;
+        dp[2] = 2;
+        int ans = 0;
+        for(int i =3;i<=n;i++){
+          dp[i]= dp[i-2] + dp[i-1];
+         // dp[n] = ans;
+        }
+        return dp[dp.size()-1];
     }
 };
