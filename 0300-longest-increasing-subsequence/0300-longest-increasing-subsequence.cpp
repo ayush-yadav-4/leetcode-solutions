15815aa4d3fa16a2a262bd8f9385ed4603238127
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-01-05 13:18:58 +0530

    Time: 614 ms (5.39%), Space: 295.4 MB (28.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-int helper(vector<int>& arr,int idx,int prev_idx,vector<vector<int>>&dp){
-   if(idx == arr.size()){
-    
-     return 0;
-   }
-   if(dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx+1];
+    int ans = 0;
 
-   int notpick = helper(arr,idx+1,prev_idx,dp);
-   int pick = 0;
-   if(prev_idx == -1 || arr[prev_idx] < arr[idx]){
-    pick = 1 +helper(arr,idx+1,idx,dp);
-   }
+    int helper(int idx, int prev_idx,vector<int>& nums, vector<vector<int>>& dp) {
+        if(idx == nums.size()){
+            return 0;
+        }
+        if(dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx+1];
+        int pick =0;
+        if(prev_idx == -1 || nums[idx] > nums[prev_idx]){
+            pick = 1 + helper(idx+1,idx,nums,dp);
+        }
+        int notpick = helper(idx+1,prev_idx,nums,dp);
 
-   return dp[idx][prev_idx+1] = max(pick , notpick);
-}
+        return dp[idx][prev_idx+1] = max(pick,notpick);
+    }
+
     int lengthOfLIS(vector<int>& nums) {
-         vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
-
-         return helper(nums,0,-1,dp);
+       vector<vector<int>>dp(nums.size(),vector<int>(nums.size(),-1));
+       return helper(0,-1, nums, dp);
+        
     }
 };
