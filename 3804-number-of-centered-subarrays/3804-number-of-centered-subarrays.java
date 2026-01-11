208376571ac4e17b40847ac49b8b5dec0d6f2d6a
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-01-11 16:43:40 +0530

    Time: 135 ms (28.49%), Space: 46.9 MB (77.22%) - LeetHub

---
+++ 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int centeredSubarrays(int[] nums) {
+         int cnt =0;
+        for(int i=0;i<nums.length;i++){
+            int sum = 0;
+            Map<Integer,Integer> mp = new HashMap<>();
+
+            for(int j=i;j<nums.length;j++){
+             sum += nums[j];
+               mp.put(nums[j],mp.getOrDefault(nums[j],0)+1);
+              if(mp.containsKey(sum)){
+                cnt++;
+             }
+            
+            
+            
+            }
+           
+        }
+        
+        return cnt;
+    }
+}
