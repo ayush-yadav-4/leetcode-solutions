7b8edda622acb69d42cd2bb6a85e3143f6737264
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-01-22 17:53:48 +0530

    Time: 114 ms (13.29%), Space: 150.1 MB (7.64%) - LeetHub

--- 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.java
+++ 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.java
@@ -1,41 +1,7 @@
 class Solution {
-//   long ans = 0;
-//   long solve(int[] nums){
-//       long sum = 0;
 
-//       for(int i=0;i<nums.length;i++){
-//           if(i%2 == 0){
-//             sum += (nums[i] * nums[i]);
-//           }
-//           else{
-//             sum -= (nums[i] * nums[i]);
-//           }
-//       }
-
-//       return sum;
-//   }
-//    void swap(int a , int b, int[] nums){
-//       int temp = nums[a];
-//       nums[a] = nums[b];
-//       nums[b] = temp;
-//    }
-//     void helper(int idx,int[] nums,Long[] dp){
-//         if(idx == nums.length) return;
-//          ans = Math.max(ans,solve(nums));
-//          if(dp[idx]!= -1) return;
-//         for(int i = idx;i<nums.length;i++){
-//            swap(idx,i,nums);
-//            helper(idx+1,nums,dp);
-//            swap(idx,i,nums);
-//         }
-//         dp[idx] = ans;
-
-//     }
     public long maxAlternatingSum(int[] nums) {
-        // Long[] dp = new Long[nums.length];
-        // Arrays.fill(dp,(long)-1);
-        // helper(0,nums,dp);
-        // return ans;
+       
 
         Long[] arr = new Long[nums.length];
         for(int i=0;i<nums.length;i++){
