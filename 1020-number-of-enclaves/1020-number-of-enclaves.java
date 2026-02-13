22fbbd70c4304e7208753205cb2f94b52fffa801
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-02-13 17:17:52 +0530

    Time: 9 ms (53.02%), Space: 55.3 MB (79.17%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -14,35 +14,38 @@ class Solution {
         for(int i=0;i<grid.length;i++){
           if(grid[i][0] == 1){
               q.add(new Pair(i,0));
+              grid[i][0] = 0;
           }
           if(grid[i][grid[0].length-1] == 1){
      q.add(new Pair(i,grid[0].length-1));
+      grid[i][grid[0].length-1] = 0;
           }
         }
         for(int i=0;i<grid[0].length;i++){
            if(grid[0][i] == 1){
               q.add(new Pair(0,i));
+               grid[0][i] = 0;
           }
           if(grid[grid.length-1][i] == 1){
             q.add(new Pair(grid.length-1,i));
+             grid[grid.length-1][i] = 0;
           }
         }
-           for(int[] a : vis){
-           Arrays.fill(a,0);
-          }
+        
 
          int rows[] = {0,0,-1,+1};
            int cols[]= {-1,+1,0,0};
            while(q.size() > 0){
              Pair p = q.poll();
-             vis[p.row][p.col] = 1;
+            
              for(int i=0;i<4;i++){
                int r = p.row + rows[i];
                int c = p.col + cols[i];
 
-               if(r>=0 && r < grid.length && c>=0 && c < grid[0].length && grid[r][c] == 1 && vis[r][c] == 0){
+               if(r>=0 && r < grid.length && c>=0 && c < grid[0].length && grid[r][c] == 1 ){
                   q.add(new Pair(r,c));
-                  vis[r][c] = 1;
+                  grid[r][c] = 0;
+                 
                  
                }
              }
@@ -51,7 +54,7 @@ class Solution {
             int cnt = 0;
           for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j] == 1 && vis[i][j] == 0){
+                if(grid[i][j] == 1){
                    cnt++;
                }
             }
