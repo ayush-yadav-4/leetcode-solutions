1af8f37750e2eba3e2157b838832fbf9d4d8f5c1
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-02-13 17:06:34 +0530

    Time: 11 ms (17.99%), Space: 57.4 MB (70%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,61 @@
+class Solution {
+          class Pair{
+        int row; int col;
+        Pair(int row, int col ){
+            this.row = row;
+            this.col = col;
+            
+        }
+    }
+    public int numEnclaves(int[][] grid) {
+        
+        Queue<Pair> q = new LinkedList<>();
+        int[][] vis = new int[grid.length][grid[0].length];;
+        for(int i=0;i<grid.length;i++){
+          if(grid[i][0] == 1){
+              q.add(new Pair(i,0));
+          }
+          if(grid[i][grid[0].length-1] == 1){
+     q.add(new Pair(i,grid[0].length-1));
+          }
+        }
+        for(int i=0;i<grid[0].length;i++){
+           if(grid[0][i] == 1){
+              q.add(new Pair(0,i));
+          }
+          if(grid[grid.length-1][i] == 1){
+            q.add(new Pair(grid.length-1,i));
+          }
+        }
+           for(int[] a : vis){
+           Arrays.fill(a,0);
+          }
+
+         int rows[] = {0,0,-1,+1};
+           int cols[]= {-1,+1,0,0};
+           while(q.size() > 0){
+             Pair p = q.poll();
+             vis[p.row][p.col] = 1;
+             for(int i=0;i<4;i++){
+               int r = p.row + rows[i];
+               int c = p.col + cols[i];
+
+               if(r>=0 && r < grid.length && c>=0 && c < grid[0].length && grid[r][c] == 1 && vis[r][c] == 0){
+                  q.add(new Pair(r,c));
+                  vis[r][c] = 1;
+                 
+               }
+             }
+
+          }
+            int cnt = 0;
+          for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j] == 1 && vis[i][j] == 0){
+                   cnt++;
+               }
+            }
+          }
+          return cnt;
+    }
+}
