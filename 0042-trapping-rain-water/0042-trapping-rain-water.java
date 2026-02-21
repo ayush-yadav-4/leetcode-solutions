eff530f9b81c6de43f67463e87540821fa1a4a14
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-02-21 23:14:59 +0530

    Time: 1 ms (60.38%), Space: 48.7 MB (24.16%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,30 +1,31 @@
 class Solution {
-    public int trap(int[] heights) {
+    public int trap(int[] nums) {
+        Stack<Integer>st = new Stack<>();
+        int[] nge = new int[nums.length];
+        int[] pge = new int[nums.length];
+        int maxi1 = 0, maxi2 = 0;
+        for(int i=0;i<nums.length;i++){
+            maxi1 = Math.max(maxi1, nums[i]);
+            maxi2 = Math.max(maxi2,nums[nums.length-i-1]);
+           pge[i] = Math.max(maxi1, nums[i]);
+           nge[nums.length-i-1] = Math.max(maxi2, nums[nums.length-i-1]);
+        }
+       
+        int sum =0;
+        for(int i=0;i<nums.length;i++){
+          
         
-        int l =0,r = heights.length-1,ans=0;
-        int maxleft = 0,maxright = 0;
-        while(l<=r){
-            if(heights[l]<=heights[r]){
-                if(maxleft>heights[l]){
-                   ans+= maxleft - heights[l];
-                   
-                
-                }
-                maxleft = Math.max(maxleft,heights[l]);
-                l++;
-            }
-                else {
-                    if(maxright>heights[r]){
-                        ans += maxright - heights[r];
-                       
-                        
-                    }
-                     maxright = Math.max(maxright,heights[r]);
-                    r--;
-                }
+            int h = Math.min(pge[i], nge[i]);
             
+            int num =  h - nums[i];
+           if(num > 0) sum += num;
+        
+
         }
 
-        return ans;
+        return sum;
+
+
+
     }
 }
