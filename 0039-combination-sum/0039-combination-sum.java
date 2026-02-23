c6aff955c9d48791f3ab9ea340efe92041d8e6c4
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-02-23 11:12:11 +0530

    Time: 3 ms (36.64%), Space: 45.6 MB (92.59%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,27 @@
 class Solution {
-    void helper( List<List<Integer>> ans, List<Integer>temp,int target,int[] nums,int idx){
-        if(idx == nums.length){
-            if(target == 0){
-            ans.add(new ArrayList<>(temp));
-            return;
-        }
+
+    void helper(int[] candidates, int target, int n, int idx , List<Integer> temp ,List<List<Integer>>ans){
+        if(target < 0) return;
+       if(idx == n){
+        if(target == 0){
+        ans.add(new ArrayList<>(temp));
         return;
         }
-        else if(target<0) return;
-        else if(target ==0){
-            ans.add(new ArrayList<>(temp));
-            return;
-        }
-        temp.add(nums[idx]);
-        helper(ans,temp,target-nums[idx],nums,idx);
-        temp.remove(temp.size()-1);
-        helper(ans,temp,target,nums,idx+1);
-        
+        return;
+       }
+       
+      temp.add(candidates[idx]);
+       helper(candidates, target-candidates[idx] , n , idx,temp,ans );
+       temp.remove(temp.size()-1);
+       helper(candidates, target, n , idx+1,temp,ans);
+
+
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer>temp = new ArrayList<>();
+        List<List<Integer>>ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
 
-        helper(ans,temp,target,candidates,0);
+        helper(candidates, target,candidates.length,0,temp,ans);
         return ans;
     }
 }
