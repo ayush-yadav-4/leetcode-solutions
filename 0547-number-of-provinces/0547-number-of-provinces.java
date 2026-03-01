1e65440780f8c09282616ca2c9c25061c0249e6a
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-03-01 11:06:50 +0530

    Time: 3 ms (31.74%), Space: 47.2 MB (88.73%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,43 +1,49 @@
 class Solution {
-    class Disjoint{
 
-        ArrayList<Integer> parent = new ArrayList<>();
-        ArrayList<Integer>Rank = new ArrayList<>();
 
-        Disjoint(int n){
-            
-         for(int i=0;i<n;i++){
-              parent.add(i);
-              Rank.add(0);
-         }
-        } 
+class Disjoint {
 
-         int findParent(int u){
-             if(u == parent.get(u)) return u;
+    ArrayList<Integer> parent = new ArrayList<>();
+    ArrayList<Integer> rank = new ArrayList<>();
 
-            int ultimateParent = findParent(parent.get(u));
-            parent.set(u, ultimateParent);
-            return ultimateParent;
-         }
-
-         void unionByRank(int u, int v){
-           int paren_u = findParent(u);
-           int paren_v = findParent(v);
-           if(paren_u == paren_v) return;
-
-           if(Rank.get(paren_u) < Rank.get(paren_v)){
-             parent.set(paren_u, paren_v);
-           }
-           else if(Rank.get(paren_v) < Rank.get(paren_u)){
-             parent.set(paren_v, paren_u);
-           }
-           else{
-              parent.set(paren_u,paren_v);
-             Rank.set(paren_v,Rank.get(paren_v)+1);
-           }
-         }
-
+    // Constructor
+    Disjoint(int n) {
+        for (int i = 0; i <= n; i++) {
+            parent.add(i);   // parent of itself
+            rank.add(0);     // initial rank = 0
         }
+    }
+
+    // Find Parent (Path Compression)
+    int Parent(int u) {
+        if (parent.get(u) == u)
+            return u;
+
+        int ultParent = Parent(parent.get(u));
+        parent.set(u, ultParent);   // path compression
+        return parent.get(u);
+    }
+
+    // Union By Rank
+    void unionByRank(int u, int v) {
+
+        int paren_u = Parent(u);
+        int paren_v = Parent(v);
+
+        if (paren_u == paren_v) return;
+
+        if (rank.get(paren_u) < rank.get(paren_v)) {
+            parent.set(paren_u, paren_v);
+        }
+        else if (rank.get(paren_u) > rank.get(paren_v)) {
+            parent.set(paren_v, paren_u);
+        }
+        else {
+            parent.set(paren_v, paren_u);
+            rank.set(paren_u, rank.get(paren_u) + 1);
+        }
+    }
+}    
     
     void DFS(int[][] mat, int i,boolean[] vis){
          vis[i] = true;
@@ -51,26 +57,20 @@ class Solution {
     public int findCircleNum(int[][] mat) {
         int cnt = 0;
         Disjoint ds = new Disjoint(mat.length);
-        boolean[] vis = new boolean[mat.length];
-        Arrays.fill(vis,false);
         for(int i=0;i<mat.length;i++){
-           for(int j=0;j<mat[i].length;j++){
-            //   if(vis[i] == false && mat[i][j] == 1){
-            //     cnt++;
-                
-            //     DFS(mat,i,vis);
-            //   }
-
-            if(mat[i][j] == 1){
-             ds.unionByRank(i,j);
-
+            for(int j =0;j<mat[0].length;j++){
+              if(mat[i][j] == 1){
+                ds.unionByRank(i,j);
+              }
             }
-           }
         }
 
         for(int i=0;i<mat.length;i++){
-          if(ds.findParent(i) == i) cnt++;
+          if(ds.parent.get(i) == i){
+            cnt++;
+          }
         }
         return cnt;
+       
     }
 }
