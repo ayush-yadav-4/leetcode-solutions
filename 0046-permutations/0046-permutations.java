167b628c75ccc9cff1e266258321ea59ed74e115
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-08-03 23:35:30 +0530

    Time: 1 ms (87.14%), Space: 45.8 MB (15.66%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,30 +1,34 @@
 class Solution {
-    void swap(int a, int b, List<Integer>ls){
-        int temp = ls.get(a);
-        ls.set(a,ls.get(b));
-        ls.set(b,temp);
-        return;
+    void swap(int a, int b, int[] nums){
+      int temp = nums[a];
+      nums[a] = nums[b];
+      nums[b] = temp;
     }
-    void helper(List<Integer> nums, int idx, int n, List<List<Integer>>ans){
+    void help(int idx , int[] nums ,List<int[]> temp,List<List<Integer>> ans ){
+     
+     if(idx == nums.length){
+       List<Integer> t =new ArrayList<>();
 
-        if(idx == n){
-            
-            ans.add(new ArrayList<>(nums));
-            return;
-        }
-        for(int i=idx;i<nums.size();i++){
-           swap(idx,i,nums);
-           helper(nums,idx+1,n,ans);
-           swap(idx,i,nums);
-        }
+       for(int a: nums){
+         t.add(a);
+       }
+       ans.add(new ArrayList<>(t));
+        return;
+     }
+
+     for(int i=idx;i<nums.length;i++){
+       swap(idx,i,nums);
+      
+       help(idx+1,nums,temp,ans);
+       swap(idx,i,nums);
+      
+     }
+     return;
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ls = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-          ls.add(nums[i]);
-        }
-         helper(ls,0,nums.length,ans);
-         return ans;
+        List<int[]> temp = new ArrayList<>();
+        help(0,nums,temp,ans);
+        return ans;
     }
 }
