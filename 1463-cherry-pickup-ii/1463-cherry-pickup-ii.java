070fade3b05a1a560c80c573c65d0a678855c663
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-08-07 23:47:44 +0530

    Time: 25 ms (66.64%), Space: 46.4 MB (62.49%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,42 +1,42 @@
 class Solution {
-    int helper(int i , int j1,int j2,int[][][] dp, int[][] grid){
-        
-        if(i>=grid.length || j1<0 || j1>=grid[0].length || j2<0 || j2>=grid[0].length){
-            return -1_000_000_000;
-        }
+    
+    int help(int i, int j1, int j2, int[][] grid, int[][][]dp){
+       if(i>= grid.length || j1 < 0 || j2 <0 || j1 >= grid[0].length || j2 >= grid[0].length){
+        return Integer.MIN_VALUE;
+    }
+      if(i == grid.length-1){
+        if(j1 == j2) return grid[i][j2];
+        else return grid[i][j1] + grid[i][j2];
+      }
 
-        if(i == grid.length-1){
-            if(j1 == j2){
-                return grid[i][j1];
-            }
-            return grid[i][j1] + grid[i][j2];
-        }
+      if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+      
+      int maxi = Integer.MIN_VALUE;
+      for (int a = -1; a <= 1; a++) {
+    for (int b = -1; b <= 1; b++) {
 
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
-          int[] d = {-1,0,+1};
-        int ans = Integer.MIN_VALUE;
-        for(int a = 0;a<3;a++){
-            for(int b=0;b<3;b++){
-                  if(j1 == j2){
-                     ans = Math.max(ans,(grid[i][j1] + helper(i+1,j1 + d[a],j2 + d[b],dp,grid)));
-                  }
-                  else{
-                    ans = Math.max(ans,(grid[i][j1]+grid[i][j2] + helper(i+1,j1 + d[a],j2 + d[b],dp,grid)));
-                  }
-            }
-        }
+        int cherries;
 
-        return dp[i][j1][j2] = ans;
+        if (j1 == j2)
+            cherries = grid[i][j1];
+        else
+            cherries = grid[i][j1] + grid[i][j2];
+
+        cherries += help(i + 1, j1 + a, j2 + b, grid, dp);
+
+        maxi = Math.max(maxi, cherries);
+    }
+}
+
+       return dp[i][j1][j2] = maxi;
     }
     public int cherryPickup(int[][] grid) {
         int[][][] dp = new int[grid.length][grid[0].length][grid[0].length];
-
-        for(int[][] arr: dp){
-             for(int[]arr2:arr){
-                Arrays.fill(arr2,-1);
-             }
-        }
-
-         return helper(0,0,grid[0].length-1,dp,grid);
+       for(int[][] a: dp){
+         for(int[] b: a){
+            Arrays.fill(b,-1);
+         }
+       }
+        return help(0,0,grid[0].length-1,grid,dp);
     }
 }
