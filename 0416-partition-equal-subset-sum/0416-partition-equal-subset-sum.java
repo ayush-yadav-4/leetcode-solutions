0afaaaaf73f69cd355f5c08d74149ad649cd5ff0
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-08-09 18:56:22 +0530

    Time: 97 ms (18.43%), Space: 72.7 MB (28.28%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,55 +1,34 @@
 class Solution {
-    boolean helper(int[] nums, int idx,boolean[][][]dp,int sum1,int sum2){
-        if(idx < 0){
-            if(sum1 == sum2) return true;
-            return false;
-        }
+    boolean help(int idx, int[] nums, int target, int[][] dp){
 
-        if(dp[idx][sum1][sum2]) return dp[idx][sum1][sum2];
-
-        return dp[idx][sum1][sum2] = helper(nums,idx-1,dp,sum1+nums[idx],sum2) || helper(nums,idx-1,dp,sum1,sum2+nums[idx]);
+        if(target == 0){
+        return true;
     }
+    if(target != 0 && idx == nums.length) return false;    
+    if(idx == nums.length || target <0) return false;
+    
+    if(dp[idx][target] != -1) return dp[idx][target] == 1;
+
+  boolean take =  help(idx+1,nums,target-nums[idx],dp);
+  boolean ntake =  help(idx+1,nums,target,dp);
+
+  dp[idx][target] = (take || ntake) ? 1: 0;
+
+  return take || ntake;
+    }
+
     public boolean canPartition(int[] nums) {
-        int sum  = 0;
-        for(int i=0;i<nums.length;i++){
-          sum+= nums[i];
-        }
-        if(sum%2 != 0 ) return false;
-        sum = sum/2;
-        boolean[][] dp = new boolean[nums.length+1][sum+1];
-
-        
-            for(boolean[]row1:dp){
-                Arrays.fill(row1,false);
-            }
-        
-    //   int idx = nums.length-1,sum1 = 0,sum2 = 0;
-    //     return helper(nums,idx,dp,sum1,sum2);
-
-        dp[0][0] = true;
-      
-        for(int idx=1;idx<=nums.length;idx++){
-            for(int sum1 = 0;sum1<=sum;sum1++){
-                
-
-                    if(sum1 == 0){
-                         dp[idx][sum1] = true;
-                    }
-                    boolean take1 = false;
-                   if(sum1 >= nums[idx-1]){
-                        take1 = dp[idx-1][sum1 - nums[idx-1]];
-                   }
-                   boolean nottake = dp[idx-1][sum1];
-                   
-
-                   dp[idx][sum1] =  take1 || nottake;
-                
-            }
-        }
-
-        
-
-        return dp[nums.length][sum];
+       int sum = 0;
+       for(int i =0;i<nums.length;i++){
+        sum += nums[i];
+       }
+       if(sum%2 != 0) return false;
+       int target = sum/2;
+       int[][] dp = new int[nums.length][target+1];
+       for(int[] a: dp){
+         Arrays.fill(a,-1);
+       }
+       return help(0,nums,target,dp);
 
 
 
