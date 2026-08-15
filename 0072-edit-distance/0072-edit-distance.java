ad18a32b5900e1ae75c69586876568e0e2661985
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-08-15 17:26:18 +0530

    Time: 6 ms (8.6%), Space: 47.1 MB (53.29%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,27 +1,47 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length()+1][word2.length()+1];
 
-        for(int i=0;i<=word1.length();i++){
-          dp[i][0] = i+1;
-        }
-        for(int i=0;i<=word2.length();i++){
-          dp[0][i] = i+1;
+    int help(int i, int j , String s, String t, int[][]dp){
+      if( j == t.length()) return s.length() - i;
+      if(i == s.length()){
+        if(j!=t.length())return t.length() - j;
+
+        return 0;
+      }
+      if(s.charAt(i) == t.charAt(j)){
+        return dp[i][j] = help(i+1, j+1,s,t,dp);
+      }
+
+      return dp[i][j] = 1 + Math.min(help(i+1,j+1,s,t,dp), Math.min(help(i+1,j,s,t,dp),help(i,j+1,s,t,dp)));
+    }
+    public int minDistance(String s, String t) {
+        int[][] dp = new int[s.length()+1][t.length()+1];
+
+        for(int[] a: dp){
+        Arrays.fill(a,-1);
         }
 
-        for(int i=1;i<=word1.length();i++){
-            for(int j=1;j<=word2.length();j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = 1 + Math.min(
-                      dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1])
-                    );
-                }
+        //return help(0,0,s,t,dp);
+
+        for(int i=0;i<=s.length();i++){
+          dp[i][0] = i; 
+        }
+         for(int j=0;j<=t.length();j++){
+          dp[0][j] = j; 
+        }
+
+        for(int i=1;i<=s.length();i++){
+            for(int j=1;j<=t.length();j++){
+              
+              if(s.charAt(i-1) == t.charAt(j-1)){
+                dp[i][j] = dp[i-1][j-1]; 
+              }
+              else{
+                dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+              }
             }
         }
 
-        return dp[word1.length()][word2.length()]-1;
+        return dp[s.length()][t.length()];
+
     }
 }
