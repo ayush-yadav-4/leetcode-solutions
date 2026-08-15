01ff278491d7a0c101a4c949eead658919d3ed53
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-08-15 17:31:08 +0530

    Time: 3 ms (98.21%), Space: 47 MB (71.77%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -7,6 +7,8 @@ class Solution {
 
         return 0;
       }
+
+      if (dp[i][j] != -1) return dp[i][j];
       if(s.charAt(i) == t.charAt(j)){
         return dp[i][j] = help(i+1, j+1,s,t,dp);
       }
@@ -20,28 +22,28 @@ class Solution {
         Arrays.fill(a,-1);
         }
 
-        //return help(0,0,s,t,dp);
+        return help(0,0,s,t,dp);
 
-        for(int i=0;i<=s.length();i++){
-          dp[i][0] = i; 
-        }
-         for(int j=0;j<=t.length();j++){
-          dp[0][j] = j; 
-        }
+        // for(int i=0;i<=s.length();i++){
+        //   dp[i][0] = i; 
+        // }
+        //  for(int j=0;j<=t.length();j++){
+        //   dp[0][j] = j; 
+        // }
 
-        for(int i=1;i<=s.length();i++){
-            for(int j=1;j<=t.length();j++){
+        // for(int i=1;i<=s.length();i++){
+        //     for(int j=1;j<=t.length();j++){
               
-              if(s.charAt(i-1) == t.charAt(j-1)){
-                dp[i][j] = dp[i-1][j-1]; 
-              }
-              else{
-                dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
-              }
-            }
-        }
+        //       if(s.charAt(i-1) == t.charAt(j-1)){
+        //         dp[i][j] = dp[i-1][j-1]; 
+        //       }
+        //       else{
+        //         dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+        //       }
+        //     }
+        // }
 
-        return dp[s.length()][t.length()];
+        // return dp[s.length()][t.length()];
 
     }
 }
