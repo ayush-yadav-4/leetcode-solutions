29768d8d0a6f442e838265fbb5f9cc66e96b0fdd
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-08-30 00:27:09 +0530

    Time: 15 ms (47.03%), Space: 46.7 MB (41.37%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,28 +1,28 @@
 class Solution {
    
     public String frequencySort(String s) {
-        Map<Character,Integer>mp = new HashMap<>();
+        Map<Character , Integer>mp = new HashMap<>();
 
         for(int i=0;i<s.length();i++){
-            mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
+          mp.put(s.charAt(i), mp.getOrDefault(s.charAt(i),0) + 1);
+
         }
-
-        List<Character> ls = new ArrayList<>(mp.keySet());
-
-        Collections.sort(ls,(a,b)->{
-            int freqa = mp.get(a);
-            int freqb = mp.get(b);
-
-            return freqb - freqa;
-        });
-
-        StringBuilder ans = new StringBuilder("");
+        ArrayList<int[]> ls = new ArrayList<>();
+        for(Map.Entry<Character , Integer> m : mp.entrySet()){
+            ls.add(new int[]{m.getKey() - 'a', m.getValue()});
+        }
+        Collections.sort(ls, (a,b)->b[1] - a[1]);
+        StringBuilder ans = new StringBuilder();
 
         for(int i=0;i<ls.size();i++){
-           for(int j = 0;j<mp.get(ls.get(i));j++){
-            ans.append(ls.get(i));
-           }
-    }
-    return ans.toString();
+            int n = mp.get((char)('a' + ls.get(i)[0]));
+
+            for(int k=0;k<n;k++){
+             ans.append((char)('a' + ls.get(i)[0]));
+            }
+           
+        }
+        return ans.toString();
+
     }
 }
