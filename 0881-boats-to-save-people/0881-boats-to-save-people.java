1e246d3ff76938f625208ca2b1ac6341b356b589
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-08-31 17:31:40 +0530

    Time: 20 ms (83.49%), Space: 56.6 MB (38.98%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i=0,j=people.length-1,cnt = 0;
+       while(i <= j ){
+          if(people[i] + people[j] <= limit){
+            cnt++;
+            i++; j--;
+          }
+          else{
+            j--; cnt++;
+          }
+       } 
+       return cnt;
+    }
+}
