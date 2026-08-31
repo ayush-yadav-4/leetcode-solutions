e7d4373045a68ca35b24131854ccc2235d5df735
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-08-31 13:25:10 +0530

    Time: 2 ms (89.03%), Space: 47.7 MB (91.63%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+      if(nums.length <=1) return true;
+      if(nums[0] == 0) return false;
+      int maxi = 0;
+        for(int i=0;i<nums.length;i++){
+         if(i > maxi) return false;
+         maxi = Math.max(maxi, (i + nums[i]));
+       
+        }
+        return true;
+    }
+}
