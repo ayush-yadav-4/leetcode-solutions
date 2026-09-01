c3bdcdd2765e260047503f2850895ca572a0cc3c
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-09-01 12:21:27 +0530

    Time: 0 ms (100%), Space: 41.7 MB (94.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -9,10 +9,16 @@ class Solution {
        return dp[n] = help(n-1,dp) + help(n-2,dp);
     }
     int climbStairs(int n) {
-       
+       if(n==0) return 1;
+       if(n==1) return 1;
        int[] dp = new int[n+1];
-       Arrays.fill(dp,-1);
-       help(n,dp);
+       //Arrays.fill(dp,-1);
+      // help(n,dp);
+       dp[0] = 1;
+       dp[1] = 1;
+       for(int i=2;i<=n;i++){
+         dp[i] = dp[i-1] + dp[i-2];
+       }
        return dp[n];
     }
 };
