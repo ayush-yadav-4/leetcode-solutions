a07e0554f47af120616fa82d4b3fa8217d844347
Ayush Indradev Yadav <officialayushyadav7@gmail.com>
2026-09-01 13:34:17 +0530

    Time: 0 ms (100%), Space: 42.9 MB (19.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,21 +1,18 @@
 class Solution {
+   
+   int help(int idx, int[] dp , int[] nums){
+    if(idx == 0) return nums[0];
+    if(idx < 0) return 0;
 
-    int help(int idx , int[] dp, int[] nums){
-        if(idx<0) return 0;
-      if(idx == 0){
-        return nums[idx];
-      }
+    if(dp[idx] != -1) return dp[idx];
 
-      if(dp[idx] != -1){
-        return dp[idx];
-      }
+    int pick = nums[idx] + help(idx-2,dp,nums);
+    int npick = help(idx-1,dp,nums);
 
-      int take = 0, ntake = 0;
-       take = nums[idx] + help(idx-2,dp,nums);
-      ntake = help(idx-1,dp,nums);
+    return dp[idx] = Math.max(pick,npick);
 
-      return dp[idx] = Math.max(take,ntake);
-    }
+   }
+    
     public int rob(int[] nums) {
         if(nums.length == 0) return 0;
         if(nums.length == 1) return nums[0];
@@ -24,7 +21,7 @@ class Solution {
          int n = nums.length-1;
         help(n,dp,nums);
        
-        if(dp[n] == -1) return 0;
+       // if(dp[n] == -1) return 0;
         return dp[n];
     }
 }
